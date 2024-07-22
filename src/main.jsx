ec311902675bbbf02550601d3b6840ab53fb7882
abhisek8094 <iasabhi41@gmail.com>
2024-07-22 23:14:12 +0530

    main commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-// src/index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import  store  from "./store/Store.jsx"; // Use named import
+import  store  from "./store/Store.jsx"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
