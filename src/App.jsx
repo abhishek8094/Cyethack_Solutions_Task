1d59b1392d95e7af2c88263629ae4df8238e4186
abhisek8094 <iasabhi41@gmail.com>
2024-07-27 08:25:34 +0530

    App

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,27 @@ import { RouterProvider } from 'react-router-dom';
 import { createBrowserRouter } from 'react-router-dom';
 import PublicRoute from "./Routes/PublicRoute"
 import PrivateRoute from "./Routes/PrivateRoute";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import Dashboard from "./Components/Dashboard";
-import List from "./pages/List"
-import ListDetails from "./pages/ListDetails"
+import List from "./pages/List";
+import ListDetails from "./pages/ListDetails";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element:<PublicRoute element={<Home />} />
   },
+
   {
     path: "/dashboard",
     element: <PrivateRoute element={<Dashboard />}/>,
   },
+
   {
     path: "/list",
     element:<List />,
   },
+  
   {
     path: "/list/details",
     element: <ListDetails/>,
